e59f78a19ea4d86fbd9aed370d8e1ef2fe533934
Aayush Chouhan <113571796+aayushchouhan0@users.noreply.github.com>
2024-01-26 10:26:07 +0530

    Update HeroBanner.jsx

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -36,9 +36,7 @@ const HeroBanner = () => {
                         src="/slide-1.png"
                         className="aspect-[16/10] md:aspect-auto object-cover"
                     />
-                    <div className="px-[15px] md:px-[40px] py-[10px] md:py-[25px] font-oswald bg-white absolute bottom-[25px] md:bottom-[75px] left-0 text-black/[0.9] text-[15px] md:text-[30px] uppercase font-medium cursor-pointer hover:opacity-90">
-                        Shop now
-                    </div>
+                  
                 </div>
 
                 <div>
@@ -46,9 +44,7 @@ const HeroBanner = () => {
                         src="/slide-2.png"
                         className="aspect-[16/10] md:aspect-auto object-cover"
                     />
-                    <div className="px-[15px] md:px-[40px] py-[10px] md:py-[25px] font-oswald bg-white absolute bottom-[25px] md:bottom-[75px] left-0 text-black/[0.9] text-[15px] md:text-[30px] uppercase font-medium cursor-pointer hover:opacity-90">
-                        Shop now
-                    </div>
+                    
                 </div>
 
                 <div>
@@ -56,9 +52,7 @@ const HeroBanner = () => {
                         src="/slide-3.png"
                         className="aspect-[16/10] md:aspect-auto object-cover"
                     />
-                    <div className="px-[15px] md:px-[40px] py-[10px] md:py-[25px] font-oswald bg-white absolute bottom-[25px] md:bottom-[75px] left-0 text-black/[0.9] text-[15px] md:text-[30px] uppercase font-medium cursor-pointer hover:opacity-90">
-                        Shop now
-                    </div>
+                   
                 </div>
             </Carousel>
         </div>
