e84cdbfe5afb328a348e7da8a07ab5b05142a115
Aayush Chouhan <113571796+aayushchouhan0@users.noreply.github.com>
2024-01-26 10:21:35 +0530

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -75,14 +75,7 @@ const Header = () => {
                 )}
 
                 <div className="flex items-center gap-2 text-black">
-                    {/* Icon start */}
-                    <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
-                        <IoMdHeartEmpty className="text-[19px] md:text-[24px]" />
-                        <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
-                            0
-                        </div>
-                    </div>
-                    {/* Icon end */}
+                  
 
                     {/* Icon start */}
                     <Link href="/cart">
